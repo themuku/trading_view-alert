236a7df77babce77a5738c5b9dd41fcf451632b0
themuku <murad.m@develops.today>
2024-06-01 22:22:37 +0400

    added push notification for mobile

--- pyautogui_version.py
+++ pyautogui_version.py
@@ -1,64 +1,59 @@
-import datetime
 import os
+
 import pyautogui
+import http.client
 import time
-import smtplib
-from email.mime.text import MIMEText
-from PIL import Image
-from dotenv import load_dotenv
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
-from tkinter import messagebox
+import urllib
 from datetime import datetime
 
+import pyautogui
+from dotenv import load_dotenv
 
 load_dotenv()
 
-
-from_email = os.getenv("FROM_EMAIL")
-to_email = os.getenv("TO_EMAIL")
-password = os.getenv("PASSWORD")
+user = os.getenv("USER")
+token = os.getenv("TOKEN")
 
 
-def send_email_alert():
-    msg = MIMEText("Custom indicator signal detected on TradingView!")
-    msg['Subject'] = 'TradingView Alert'
-    msg['From'] = from_email
-    msg['To'] = to_email
+def send_push(msg):
+    conn = http.client.HTTPSConnection("api.pushover.net:443")
+    conn.request("POST", "/1/messages.json",
+                 urllib.parse.urlencode({
+                     "token": token,
+                     "user": user,
+                     "title": "Trading",
+                     "sound": "echo",
+                     "priority": 2,
+                     "retry": 30,
+                     "expire": 10800,
+                     "message": msg,
+                 }), { "Content-type": "application/x-www-form-urlencoded" })
+    conn.getresponse()
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
-        server.ehlo()
-        server.starttls()
-        server.ehlo()
-        server.login(from_email, password)
-        server.sendmail(from_email, to_email, msg.as_string())
 
-
-Tk().withdraw()
-image_path = askopenfilename()
+buy_img = "./buy.jpg"
+sell_img = "./sell.jpg"
 
 region = (0, 0, 1920, 1080)
 
-
-try:
-    image = Image.open(image_path)
-except IOError:
-    print(f"Unable to open image file: {image_path}")
-    exit(1)
 
 try:
     while True:
         screenshot = pyautogui.screenshot(region=region)
 
         try:
-            signal_location = pyautogui.locate(image_path, screenshot, confidence=0.8)
+            buy_signal_location = pyautogui.locate(buy_img, screenshot, confidence=0.8)
+            sell_signal_location = pyautogui.locate(sell_img, screenshot, confidence=0.8)
             indicator_appeared = datetime.now()
         except pyautogui.ImageNotFoundException:
-            signal_location = None
+            buy_signal_location = None
+            sell_signal_location = None
 
-        if signal_location:
-            send_email_alert()
-            messagebox.showinfo("Indicator appeared", f"The indicator have just appeared in the chart at {indicator_appeared}")
+        if buy_signal_location:
+            send_push("Buy signal")
+
+        if sell_signal_location:
+            send_push("Sell signal")
 
         time.sleep(60)
 except KeyboardInterrupt:
